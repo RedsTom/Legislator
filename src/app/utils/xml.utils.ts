13bcfa9504fee4b4f4547314cb34249923f5fd51
RedsTom <tom.frefre.13@gmail.com>
2025-05-05 10:23:16 +0200

    Améliorations mineures

--- src/app/utils/xml.utils.ts
+++ src/app/utils/xml.utils.ts
@@ -1,12 +1,12 @@
 export function parseText(xml: string): string {
   return xml
-    .replace(/<italique>/g, "*")
-    .replace(/<\/italique>/g, "*")
-    .replace(/<gras>/g, "**")
-    .replace(/<\/gras>/g, "**")
-    .replace(/<exposant>/g, "<sup>")
-    .replace(/<\/exposant>/g, "</sup>")
-    .replace(/<br\/>/g, "\n\n")
-    .replace(/<lienAdt[^>]*>/g, "[")
-    .replace(/<\/lienAdt>/g, "]");
+    .replace(/<italique.*?>/g, "*")
+    .replace(/<\/italique.*?>/g, "*")
+    .replace(/<gras.*?>/g, "**")
+    .replace(/<\/gras.*?>/g, "**")
+    .replace(/<exposant.*?>/g, "<sup>")
+    .replace(/<\/exposant.*?>/g, "</sup>")
+    .replace(/<br.*?\/>/g, "\n\n")
+    .replace(/<lienAdt[^>]*.*?>/g, "[")
+    .replace(/<\/lienAdt.*?>/g, "]");
 }
